ad29ccfba440bef6edf2a4882e54607fb3693437
Promise30 <promiseadeagbo@gmail.com>
2024-07-18 11:57:12 +0100

    inital request dto file for removing user from role

---
+++ BloggingAPI/Domain/Entities/Dtos/Requests/Auth/RemoveUserFromRoleDto.cs
@@ -0,0 +1,14 @@
+﻿using BloggingAPI.Infrastructure.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace BloggingAPI.Domain.Entities.Dtos.Requests.Auth
+{
+    public record RemoveUserFromRoleDto
+    (
+        [Required(ErrorMessage = ("Email field is required"))]
+        [EmailAddress(ErrorMessage = "A valid email address is required")]
+        string Email,
+        [ValidRoles(ErrorMessage = "Invalid role specified")]
+        ICollection<string> Roles
+    );
+}
