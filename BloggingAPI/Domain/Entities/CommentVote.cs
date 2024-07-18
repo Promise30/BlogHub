201a0122a8ada77b8639eb73d10f62e88f04ab4d
Promise30 <promiseadeagbo@gmail.com>
2024-07-18 11:58:13 +0100

    initial class definition for commentVote

---
+++ BloggingAPI/Domain/Entities/CommentVote.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BloggingAPI.Domain.Entities
+{
+    public class CommentVote
+    {
+        [Key]
+        public int Id { get; set; }
+        public int CommentId { get; set; }
+        public virtual Comment Comment { get; set; }
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
+        public bool? IsUpVote { get; set; }
+    }
+}
