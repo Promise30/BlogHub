aef6e019479d5c7ccff173bc822846909399ef4a
Promise30 <promiseadeagbo@gmail.com>
2024-07-18 11:36:09 +0100

    added extension method for user to userResponse class

--- BloggingAPI/Extensions/MappingExtensions.cs
+++ BloggingAPI/Extensions/MappingExtensions.cs
@@ -1,10 +1,26 @@
 ﻿using BloggingAPI.Domain.Entities;
+using BloggingAPI.Domain.Entities.Dtos.Responses;
 using BloggingAPI.Domain.Entities.Dtos.Responses.Posts;
 
 namespace BloggingAPI.Extensions
 {
     public static class MappingExtensions
     {
+        public static UserResponseDto MapToUserResponseDto(this User user, IEnumerable<string> roles)
+        {
+            return new UserResponseDto
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+                Roles = roles.ToList(),
+                DateCreated = user.DateCreated,
+                DateModified = user.DateModified,
+            };
+        }
         public static PostWithCommentsDto MapToPostDto(this Post post)
         {
             return new PostWithCommentsDto
@@ -21,7 +37,10 @@ namespace BloggingAPI.Extensions
                     Id = c.Id,
                     Content = c.Content,
                     Author = c.Author,
+                    UpVoteCount = c.UpVoteCount,
+                    DownVoteCount = c.DownVoteCount,
                     PublishedOn = c.PublishedOn
+
                 }).ToList(),
             };
 
