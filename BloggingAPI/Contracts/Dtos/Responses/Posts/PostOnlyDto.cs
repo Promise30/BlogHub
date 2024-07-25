991916445b4a78be6af65ce30fafc5095bd9f646
Promise30 <promiseadeagbo@gmail.com>
2024-07-25 02:06:49 +0100

    modified the datatype of PublishedOn property

--- BloggingAPI/Contracts/Dtos/Responses/Posts/PostOnlyDto.cs
+++ BloggingAPI/Contracts/Dtos/Responses/Posts/PostOnlyDto.cs
@@ -8,6 +8,6 @@
         public string Author { get; init; }
         public string Category { get; init; }
         public string PostImageUrl { get; init; }
-        public string PublishedOn { get; init; }
+        public DateTime PublishedOn { get; init; }
     }
 }
