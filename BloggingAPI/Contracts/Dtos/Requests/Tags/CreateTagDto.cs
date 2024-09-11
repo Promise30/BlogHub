db3610a4c2ff0bea89f6752fc66700f3e98e0a56
Promise30 <promiseadeagbo@gmail.com>
2024-09-11 14:32:30 +0100

    Added request dto files and creating and updating tags

---
+++ BloggingAPI/Contracts/Dtos/Requests/Tags/CreateTagDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BloggingAPI.Contracts.Dtos.Requests.Tags
+{
+    public class CreateTagDto
+    {
+        [Required(ErrorMessage = "A tag name is required")]
+        public string Name { get; set; }
+    }
+}
