064d7df50c75b7ff5dfaef344a873ea0db9f2196
Promise30 <promiseadeagbo@gmail.com>
2024-09-11 14:46:36 +0100

    Updated project DbContext file

--- BloggingAPI/Persistence/ApplicationDbContext.cs
+++ BloggingAPI/Persistence/ApplicationDbContext.cs
@@ -4,19 +4,21 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BloggingAPI.Persistence;
-public class ApplicationDbContext : IdentityDbContext<User>
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
     }
     public DbSet<Post> BlogPosts { get; set; }
     public DbSet<Comment> PostComments { get; set; }
+    public DbSet<PostTag> PostTags { get; set; }
+    public DbSet<Tag> Tags { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         base.OnModelCreating(modelBuilder);
-      
+        
         modelBuilder.Entity<IdentityRole>().HasData(
          new IdentityRole
          {
