aa6db7e0cc5f3393e1c445f1b756b80a47400430
Promise30 <promiseadeagbo@gmail.com>
2024-09-11 14:38:38 +0100

    Updated post related requests dto files

---
+++ BloggingAPI/Contracts/Dtos/Requests/Posts/UpdatePostCoverImageDto.cs
@@ -0,0 +1,12 @@
+﻿using BloggingAPI.Contracts.Validations;
+
+namespace BloggingAPI.Contracts.Dtos.Requests.Posts
+{
+    public class UpdatePostCoverImageDto
+    {
+
+        [AllowedFileExtensions(new[] { ".jpg", ".png", ".jpeg" }, ErrorMessage = "Only JPG, JPEG and PNG files are allowed")]
+        [MaxFileSize(2 * 1024 * 1024, ErrorMessage = "File size must not exceed 2 MB")]
+        public IFormFile? PostCoverImage { get; set; }
+    }
+}
