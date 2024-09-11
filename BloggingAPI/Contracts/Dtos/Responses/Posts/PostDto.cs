24b05972463f11940ba6bf54600cf17554133fa0
Promise30 <promiseadeagbo@gmail.com>
2024-09-11 14:33:32 +0100

    Added dtos response files for post entity

---
+++ BloggingAPI/Contracts/Dtos/Responses/Posts/PostDto.cs
@@ -0,0 +1,15 @@
+﻿using BloggingAPI.Contracts.Dtos.Requests.Tags;
+
+namespace BloggingAPI.Contracts.Dtos.Responses.Posts
+{
+    public class PostDto
+    {
+        public int Id { get; init; }
+        public string Title { get; init; }
+        public string? Content { get; init; }
+        public string Author { get; init; }
+        public IEnumerable<string>? Tags { get; init; }
+        public string? PostImageUrl { get; init; }
+        public DateTime PublishedOn { get; init; }
+    }
+}
