e784a160a722581c9a9771598efb5182472ce041
Promise30 <promiseadeagbo@gmail.com>
2024-09-21 16:28:22 +0100

    Added data annotations of model properties

--- BloggingAPI/Contracts/Dtos/Requests/Auth/ChangeEmailDto.cs
+++ BloggingAPI/Contracts/Dtos/Requests/Auth/ChangeEmailDto.cs
@@ -4,7 +4,8 @@ namespace BloggingAPI.Contracts.Dtos.Requests.Auth
 {
     public class ChangeEmailDto
     {
-        
+        [Required(ErrorMessage = "The current email address field is required")]
+        public string CurrentEmail { get; set; }
         [Required(ErrorMessage = "The new email field is required")]
         public string NewEmail { get; set; }
     }
--- BloggingAPI/Contracts/Dtos/Requests/Auth/PasswordResetDto.cs
+++ BloggingAPI/Contracts/Dtos/Requests/Auth/PasswordResetDto.cs
@@ -9,9 +9,9 @@ namespace BloggingAPI.Contracts.Dtos.Requests.Auth
         [EmailAddress(ErrorMessage = "A valid email address is required")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Password field is required")]
-        public string Password { get; set; }
+        public string NewPassword { get; set; }
         [Required(ErrorMessage = "Password confirmation field is required")]
-        [PasswordConfirmationValidation("Password", ErrorMessage = "Password fields do not match")]
+        [PasswordConfirmationValidation("NewPassword", ErrorMessage = "Password fields do not match")]
         public string ConfirmPassword { get; set; }
         [Required(ErrorMessage = "Token field is required")]
         public string Token { get; set; }
