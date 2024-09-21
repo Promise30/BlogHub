50aba08129064d38ad7be8cabfc1c8745bfb2454
Promise30 <promiseadeagbo@gmail.com>
2024-09-21 16:33:32 +0100

    Modified existing methods and added new methods

--- BloggingAPI/Services/Interface/IBloggingService.cs
+++ BloggingAPI/Services/Interface/IBloggingService.cs
@@ -2,6 +2,7 @@
 using BloggingAPI.Contracts.Dtos.Requests.Posts;
 using BloggingAPI.Contracts.Dtos.Requests.Tags;
 using BloggingAPI.Contracts.Dtos.Responses;
+using BloggingAPI.Contracts.Dtos.Responses.Comments;
 using BloggingAPI.Contracts.Dtos.Responses.Posts;
 using BloggingAPI.Contracts.Dtos.Responses.Tags;
 using BloggingAPI.Domain.Entities;
@@ -17,22 +18,22 @@ namespace BloggingAPI.Services.Interface
         Task<ApiResponse<(IEnumerable<PostDto> posts, MetaData metaData)>> GetAllUserPostsAsync(PostParameters postParameters);
         Task<ApiResponse<NewPostDto>> CreatePostAsync(CreatePostDto post);
         Task<ApiResponse<object>> UpdatePostAsync(int postId, UpdatePostDto updatePostDto);
-        Task<ApiResponse<object>> UpdatePostCoverImageAsync(int postId, UpdatePostCoverImageDto updatePostCoverImageDto);
+        Task<ApiResponse<PostDto>> UpdatePostCoverImageAsync(int postId, UpdatePostCoverImageDto updatePostCoverImageDto);
         Task<ApiResponse<object>> DeletePostAsync(int postId);
         
         // Comments
         Task<ApiResponse<(IEnumerable<CommentDto> comments, MetaData metaData)>> GetAllCommentsForPostAsync(int postId, CommentParameters commentParameters);
-        Task<ApiResponse<CommentDto>> GetCommentForPostAsync(int postId, int commentId);
+        Task<ApiResponse<CommentDto>> GetCommentForPostAsync(int commentId);
         Task<ApiResponse<CommentDto>> CreateCommentForPostAsync(int postId, CreateCommentDto createCommentDto);
-        Task<ApiResponse<object>> UpdateCommentForPostAsync(int postId, int commentId, UpdateCommentDto updateCommentDto);
-        Task<ApiResponse<object>> DeleteCommentForPostAsync(int postId, int commentId);
-        Task<ApiResponse<object>> VoteCommentAsync(int postId, int commentId, bool? isUpVote);
+        Task<ApiResponse<CommentDto>> UpdateCommentForPostAsync(int commentId, UpdateCommentDto updateCommentDto);
+        Task<ApiResponse<object>> DeleteCommentForPostAsync(int commentId);
+        Task<ApiResponse<CommentVoteDto>> VoteCommentAsync(int commentId, bool? isUpVote);
 
         // Tags
         Task<ApiResponse<TagDto>> CreateTagAsync(CreateTagDto createTag);
         Task<ApiResponse<IEnumerable<TagDto>>> GetAllTagsAsync();
         Task<ApiResponse<TagDto>> GetTagByIdAsync(int tagId);
-        Task<ApiResponse<IEnumerable<PostDetailDto>>> GetAllPostsForTagAsync(int tagId);
+        Task<ApiResponse<IEnumerable<PostDto>>> GetAllPostsForTagAsync(int tagId);
         Task<ApiResponse<object>> UpdateTagAsync(int tagId, UpdateTagDto tagDto);
         Task<ApiResponse<object>> DeleteTagAsync(int tagId);
     }
--- BloggingAPI/Services/Interface/IRedisCacheService.cs
+++ BloggingAPI/Services/Interface/IRedisCacheService.cs
@@ -2,8 +2,8 @@
 {
     public interface IRedisCacheService
     {
-        T GetCachedData<T>(string key);
-        void RemoveCache(string key);
-        void SetCachedData<T>(string key, T data, TimeSpan cacheDuration);
+        Task<T> GetCachedDataAsync<T>(string key);
+        Task RemoveCacheAsync(string key);
+        Task SetCachedDataAsync<T>(string key, T data, TimeSpan cacheDuration);
     }
 }
