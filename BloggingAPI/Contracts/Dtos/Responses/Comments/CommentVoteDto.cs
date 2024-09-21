06ce850583d935b71b9ad37fe23a0673dba533a0
Promise30 <promiseadeagbo@gmail.com>
2024-09-21 16:28:47 +0100

    Added response class for comment vote operation

---
+++ BloggingAPI/Contracts/Dtos/Responses/Comments/CommentVoteDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BloggingAPI.Contracts.Dtos.Responses.Comments
+{
+    public class CommentVoteDto
+    {
+        public int CommentId { get; set; }
+        public bool? IsUpVote { get; set; }
+        public int UpvoteCount { get; set; }
+        public int DownvoteCount { get; set; }
+    }
+}
